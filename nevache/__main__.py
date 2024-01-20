81b30e712dc2a00a5eef6905b3ce77d3d457e349
Rémi Côme <remi.come@odit-e.com>
2024-01-20 17:23:44 +0100

    feat: sort paiements alphabetically

--- nevache/__main__.py
+++ nevache/__main__.py
@@ -15,5 +15,7 @@ negative_balance = sum(value for value in balances.values() if value < 0)
 assert positive_balance + negative_balance < 1e-4
 assert abs(sum(paiement.value for paiement in paiements) - positive_balance) < 1e-4
 
-for paiement in paiements:
+paiement_strings = (repr(paiement) for paiement in paiements)
+
+for paiement in sorted(paiement_strings):
     print(paiement)
